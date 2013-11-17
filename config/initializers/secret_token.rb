1434bbef566415bbae72e49ef68cf090ed9ba2c3
i8code <rsteinbergis@gmail.com>
2013-11-17 14:34:07 -0500

    Initial upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapDraft::Application.config.secret_key_base = '2693223f57e93c6c416d0d733502e90fabec8bafacec80ac5d1220889f18b5b55d6372c7d4f07a5004c3328431fc23063c17118720780bfd24fd79b95d707b41'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MapDraft::Application.config.session_store :cookie_store, key: '_map_draft_session'
